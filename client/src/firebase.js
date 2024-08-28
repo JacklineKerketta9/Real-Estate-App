e320cbf8bf7e37e698561eebc0ea2aa3067c361d
JacklineKerketta9 <baddiejackline@gmail.com>
2024-08-28 23:33:48 +0530

    Firebase config

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -6,11 +6,11 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "realestate-da26d.firebaseapp.com",
-  projectId: "realestate-da26d",
-  storageBucket: "realestate-da26d.appspot.com",
-  messagingSenderId: "931855021140",
-  appId: "1:931855021140:web:cca1627d472a3e356ca9c1"
+  authDomain: "real-estate-ccf9c.firebaseapp.com",
+  projectId: "real-estate-ccf9c",
+  storageBucket: "real-estate-ccf9c.appspot.com",
+  messagingSenderId: "864808194060",
+  appId: "1:864808194060:web:1b829ae18f32e460f81e29"
 };
 
 // Initialize Firebase
